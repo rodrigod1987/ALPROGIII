f834e3a6c3ea8b574d010345c9e0f25371c26564
rodrigod1987 <rodrigod1987@gmail.com>
2018-10-01 17:38:25 -0300

    Criação do Teste para Quicksort com particionamento

--- Aula-5/Exe-2/src/QuickSort.java
+++ Aula-5/Exe-2/src/QuickSort.java
@@ -4,21 +4,22 @@ public class QuickSort<T> {
     {
         if (low < high) {
             int partitioned = partition(list, low, high);
-            sort(list, low, partitioned-1);
+            sort(list, low, partitioned);
             sort(list, partitioned+1, high);
         }
         return list;
     }
 
-    private int partition(Comparable<T>[] list, int low, int high) {
-        int first = low;
-        int last = low;
-        T pivo = (T)list[high-1];
+    private int partition(Comparable<T>[] list, int start, int end) {
+        int first = start;
+        int last = start;
+        T pivo = (T)list[end-1];
 
         for (int i = 0; i < list.length - 1; i++) {
             if (list[i].compareTo(pivo) > 0)
                 last -= 1;
             else {
+                last -= 1;
                 first += 1;
                 T aux = (T) list[first - 1];
                 list[first - 1] = list[i];
